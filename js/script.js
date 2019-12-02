3e8c5afd1e12b068c2b7eefa9f7c0653e38eaf6c
Antoine Durussel <antoine.durussel@gmail.com>
2019-12-02 19:55:10 +0100

    FEAT pokedex.html
    I added the name of the pokemon and a description

--- js/script.js
+++ js/script.js
@@ -1,3 +1,41 @@
+window.onload = function() {    
+
+    const buttonRight = document.getElementById('button_right');
+    const buttonLeft = document.getElementById('button_left');
+    const buttonCenter = document.getElementById('button_center');
+    let pokemonNumber = 1;
+    
+    buttonRight.addEventListener("click", function() {
+        //Jusqu'à la 4ème génération faut pas pousser
+        if(pokemonNumber < 493) { 
+            pokemonNumber++;
+            getPokemons(pokemonNumber);
+        } else if (pokemonNumber === 493) {
+            pokemonNumber = 1;
+            getPokemons(pokemonNumber);
+        }
+    })
+
+    buttonLeft.addEventListener("click", function(){
+        if(pokemonNumber > 1) {
+            pokemonNumber--;
+            getPokemons(pokemonNumber);
+        } else if (pokemonNumber === 1) {
+            pokemonNumber = 493;
+            getPokemons(pokemonNumber);
+        }
+    })
+
+    buttonCenter.addEventListener("click", function(){
+        requestAnimationFrame(displayDetails, 3000);
+    })
+
+    
+
+    getPokemons(pokemonNumber);
+
+}
+
 function getPokemons(pokemonNumber) {
 
     const xhr = new XMLHttpRequest();
@@ -5,11 +43,14 @@ function getPokemons(pokemonNumber) {
         if(xhr.readyState === 4) {
 
             const pokemon = JSON.parse(xhr.responseText);
-            const pokemonName = pokemon.name;
+            let pokemonName = pokemon.name;        
+            newPokemonName = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1);
             const pokemonImg = pokemon.sprites.front_default;
             
             const pokedexImg = document.getElementById('pokedex_img');
-            pokedexImg.innerHTML = "<img src='" + pokemonImg + "' alt='" + pokemonName + "' style='width: 100%'></img>"
+            const pokedexName = document.getElementById('pokedex_name');
+            pokedexImg.innerHTML = "<img src='" + pokemonImg + "' alt='" + newPokemonName + "' style='width: 100%'></img>";
+            pokedexName.innerHTML = newPokemonName;
 
         }
     }
@@ -18,23 +59,8 @@ function getPokemons(pokemonNumber) {
 
 }
 
-window.onload = function() {    
-
-    const buttonRight = document.getElementById('button_right');
-    const buttonLeft = document.getElementById('button_left');
-    let pokemonNumber = 1;
-    
-    buttonRight.addEventListener("click", function() {
-        pokemonNumber++;
-        getPokemons(pokemonNumber);
-    })
-
-    buttonLeft.addEventListener("click", function(){
-        pokemonNumber--;
-        getPokemons(pokemonNumber);
-    })
-
-    getPokemons(pokemonNumber);
-
+function displayDetails() {
+    const details = document.getElementById('pokedex_details');
+    details.style.display = "block";
 }
 
